4d4df81b28a7ee6e4ef99c6d477dc53915e9886c
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-11-11 14:41:37 +0100

    chore: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import { Typography } from './components';
+import { Grid, Typography } from './components';
+
 
 function App() {
+  // https://kidadl.com/quotes/best-samuel-l-jackson-quotes-from-the-iconic-actor-and-producer
   return (
-    <div className='p-4'>
-      <Typography variant="h1">Hello!</Typography>
+    <Grid m={4} cols={2} gap={10}>
+      <div>
+        <Typography variant="h1">Hello!</Typography>
+      </div>
 
-      {/* https://kidadl.com/quotes/best-samuel-l-jackson-quotes-from-the-iconic-actor-and-producer */}
-      <Typography>
-        If you have an opportunity to use your voice you should use it.
-      </Typography>
+      <div>
+        <Typography>
+          If you have an opportunity to use your voice you should use it.
+        </Typography>
+      </div>
 
-      <Typography variant='strong'>
-        The best advice that was given to me was that I had to be 10 times smarter, braver and more polite to be equal. So I did.
-      </Typography>
-    </div>
+      <div>
+        <Typography variant='strong'>
+          The best advice that was given to me was that I had to be 10 times smarter, braver and more polite to be equal. So I did.
+        </Typography>
+      </div>
+    </Grid>
   )
 }
 
