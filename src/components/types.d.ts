fa65e3a823d9ee8e9381bb43ca432f610adb5108
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-11-11 14:40:44 +0100

    fix: correct padding type

--- src/components/types.d.ts
+++ src/components/types.d.ts
@@ -22,7 +22,7 @@ export type PropsWithClassName<P> = P & { className?: string };
 
 export type MarginName = 'm' | 'mt' | 'mr' | 'mb' | 'ml' | 'mx' | 'my';
 
-export type PaddingName = 'm' | 'mt' | 'mr' | 'mb' | 'ml' | 'mx' | 'my';
+export type PaddingName = 'p' | 'pt' | 'pr' | 'pb' | 'pl' | 'px' | 'py';
 
 export type NumericalSize = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
